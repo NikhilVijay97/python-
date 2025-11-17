bb9ac6b6c24f8e9ce2ab0f320e6c23aa5a6122c0
Nikhil Vijayan <nikhilvijayan2004@gmail.com>
2025-11-17 16:00:21 +0530

    Add files via upload

---
+++ file3.py
@@ -0,0 +1,9 @@
+filename = "bbb.txt"
+lines_list = []
+
+with open("bbb.txt", "r") as file:
+    for line in file:
+        lines_list.append(line.strip())
+
+print("Lines in the list:")
+print(lines_list)
